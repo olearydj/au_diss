d58949442894c63bd439495740537d08195f8140
Dan O'Leary <oleary.dj@gmail.com>
2023-08-06 12:14:24 -0500

    first pass at combined reports

--- parse_xml/generate_combined_reports.py
+++ parse_xml/generate_combined_reports.py
@@ -1,36 +1,57 @@
 # parse all 1???.md files and generate combined report
 
+import glob
 import os
 from rich.console import Console
 from rich.traceback import install
 from rich.markdown import Markdown
+from parse_xml import convert_to_seconds
+from parse_xml import generate_markdown_report
 
 install()  # install rich traceback handler
 console = Console(highlight=False)
 
 
-def main(root, first, last, verbose=False):
+def main(reports, verbose=True):
     """build combined reports for the specified files and root dir"""
-    for p_num in range(first, last + 1):
-        md_file = f"{p_num}.md"
-        md_file_path = os.path.join(root, md_file)
-        if os.path.isfile(md_file_path):
+    for report in reports:
+        report_num, ext = os.path.splitext(os.path.basename(report))
+        report_dir = report.split("/")[-2]
+        md_file = report_num + ext
+        md_file_path = report
+
+        try:
+            # extract comments from general observations & feedback
             console.print(
-                f'\n:gear: [bold green]running:[/] :pencil:[royal_blue1]{md_file}[/] in :open_file_folder:[blue_violet]{root.split("/")[-2]}[/]'
+                f"\n:gear: [bold green]running:[/] :pencil:[royal_blue1]{md_file}[/] in :open_file_folder:[blue_violet]{report_dir}[/]"
             )
             comments = extract_comments(md_file_path)
-            if verbose:
-                if verbose == "md":
-                    md = Markdown("\n".join(comments))
-                    console.print(md)
-                else:
-                    for line in comments:
-                        print(line, end="")
-            console.print(f"\n[green]process [/]{p_num}-learn.md")
-            console.print(f"[green]process [/]{p_num}-recall.md")
-        else:
+            combined_report = ""
+
+            # extract data from xml files - video logs
+            for phase in ["Learn", "Recall"]:
+                xml_file = f"{report_num}-{phase}.xml"
+
+                # glob returns a list of matching dirs, assume first is correct
+                data_dir = glob.glob(os.path.join(DATA_ROOT_DIR, f"{report_num}-*"))[0]
+                xml_path = os.path.join(data_dir, "videos", xml_file)
+                console.print(f"\n[green]process [/]{xml_file} in {xml_path}")
+
+                xml_report = generate_markdown_report(xml_path)
+                combined_report = xml_report + "".join(comments)
+
+                # print result to console
+                if verbose:
+                    if verbose == "md":
+                        md = Markdown(combined_report)
+                        console.print(md)
+                    else:
+                        for line in combined_report:
+                            print(line, end="")
+
+        except:
             console.print(
-                f"\n:police_car_light: [bold red]{md_file} does not exist[/] :police_car_light:"
+                f"\n:police_car_light: error on [bold red]{md_file}[/] :police_car_light:"
             )
 
 
@@ -70,6 +91,7 @@ def extract_comments(md_file_path):
 
 
 if __name__ == "__main__":
-    dev_root = "/Users/djo/dev/au/au_diss/reports/"
-    start, stop = 1001, 1006
-    main(dev_root, start, stop)
+    REPORT_DIR = "/Users/djo/dev/au/au_diss/reports/"
+    DATA_ROOT_DIR = "/Volumes/ThunderBay mini/Research Master/data/"
+    report_files = sorted(glob.glob(os.path.join(REPORT_DIR, "????.md")))[:3]
+    main(report_files)
