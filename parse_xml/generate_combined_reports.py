7f433c8442428be16109be3d7e3df05eb8ba3c99
Dan O'Leary <oleary.dj@gmail.com>
2023-08-06 15:18:10 -0500

    checkpoint: reports correct thru phase notes

--- parse_xml/generate_combined_reports.py
+++ parse_xml/generate_combined_reports.py
@@ -14,45 +14,66 @@ console = Console(highlight=False)
 
 def main(reports, verbose=True):
     """build combined reports for the specified files and root dir"""
+    # print("*** in main\n")
     for report in reports:
         report_num, ext = os.path.splitext(os.path.basename(report))
         report_dir = report.split("/")[-2]
         md_file = report_num + ext
         md_file_path = report
 
+        # print("*** ", report_num, report_dir, md_file_path, sep="   ")
+
         try:
             # extract comments from general observations & feedback
             console.print(
-                f"\n:gear: [bold green]running:[/] :pencil:[royal_blue1]{md_file}[/] in :open_file_folder:[blue_violet]{report_dir}[/]"
+                f":gear: [bold green]running:[/] :pencil:[royal_blue1]{md_file}[/] in :open_file_folder:[blue_violet]{report_dir}[/]"
             )
             comments = extract_comments(md_file_path)
-            combined_report = ""
 
             # extract data from xml files - video logs
-            for phase in ["Learn", "Recall"]:
-                xml_file = f"{report_num}-{phase}.xml"
+            for phase_num, phase_name in enumerate(["Learn", "Recall"], start=1):
+                # print(f"*** building XML report for Phase {phase_num}: {phase_name}")
+                report_header = (
+                    f"# Trial {report_num}\n\n"
+                    "## Participant Information\n\n"
+                    "*Add information from xls demographics page, including treatment, date, etc.*\n\n"
+                    "## General Notes\n"
+                    f"{''.join(comments)}"
+                )
+
+                if phase_num == 1:
+                    combined_report = report_header
+
+                combined_report += f"\n## Phase {phase_num}: {'Learning' if phase_num == 1 else phase_name}\n\n"
+
+                xml_file = f"{report_num}-{phase_name}.xml"
+                # print(f"*** processing {xml_file}")
 
                 # glob returns a list of matching dirs, assume first is correct
                 data_dir = glob.glob(os.path.join(DATA_ROOT_DIR, f"{report_num}-*"))[0]
                 xml_path = os.path.join(data_dir, "videos", xml_file)
-                console.print(f"\n[green]process [/]{xml_file} in {xml_path}")
+                console.print(f"[green]process [/]{xml_file} in {xml_path}\n")
 
+                # print(f"*** generating markdown report\n")
                 xml_report = generate_markdown_report(xml_path)
-                combined_report = xml_report + "".join(comments)
+                for sec_name, sec_data in xml_report.items():
+                    # print(f"*** combining report: {sec_name}")
+                    combined_report += sec_data
 
-                # print result to console
-                if verbose:
-                    if verbose == "md":
-                        md = Markdown(combined_report)
-                        console.print(md)
-                    else:
-                        for line in combined_report:
-                            print(line, end="")
+            # print result to console
+            if verbose:
+                if verbose == "md":
+                    md = Markdown(combined_report)
+                    console.print(md)
+                else:
+                    for line in combined_report:
+                        print(line, end="")
 
-        except:
+        except Exception as e:
             console.print(
                 f"\n:police_car_light: error on [bold red]{md_file}[/] :police_car_light:"
             )
+            console.print_exception()
 
 
 def extract_comments(md_file_path):
@@ -67,6 +88,9 @@ def extract_comments(md_file_path):
     Returns:
         md_out (list[str]): Processed lines.
     """
+    # print("*** in extract_comments")
+    # print(f"*** extracting comments from: {md_file_path}\n")
+
     with open(md_file_path, "r") as md_input:
         md_in = md_input.readlines()
 
@@ -83,15 +107,16 @@ def extract_comments(md_file_path):
                 # fix notes that aren't bulleted
                 line = "- " + line
             if line.startswith("##"):
-                line = "\n" + line
+                line = "\n#" + line
             md_out.append(line)
             # print(line, end="")
 
+    # print("*** leaving extract_comments\n")
     return md_out
 
 
 if __name__ == "__main__":
     REPORT_DIR = "/Users/djo/dev/au/au_diss/reports/"
     DATA_ROOT_DIR = "/Volumes/ThunderBay mini/Research Master/data/"
-    report_files = sorted(glob.glob(os.path.join(REPORT_DIR, "????.md")))[:3]
+    report_files = sorted(glob.glob(os.path.join(REPORT_DIR, "????.md")))[:1]
     main(report_files)
