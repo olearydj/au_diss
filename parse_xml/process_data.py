41601d4e4360d63499daad3b82d3dc93ec25eb3b
Dan O'Leary <oleary.dj@gmail.com>
2023-09-02 10:01:49 -0500

    feat: extract comments

--- parse_xml/process_data.py
+++ parse_xml/process_data.py
@@ -46,12 +46,61 @@ def init_logging():
     return logging.getLogger("rich")
 
 
+def extract_comments(md_file_path):
+    """
+    Extracts the h2 elements and underlying comments from hand-transcribed
+    markdown notes (i.e., 1???.md). Ensures all notes are prefixed with "- ",
+    and all h2 elements are preceded by one blank line.
+
+    Args:
+        md_file_path (Path): Path to the markdown file to extract comments from.
+
+    Returns:
+        md_out (list[str]): Processed lines.
+    """
+    logger.info(f"*** Extracting comments from: {md_file_path}")
+
+    with open(md_file_path, "r") as md_input:
+        md_in = md_input.readlines()
+
+    h2 = False
+    md_out = []
+
+    for line in md_in:
+        if line.startswith("##"):
+            h2 = True
+        elif line.isspace():
+            h2 = False
+        if h2:
+            if not line.startswith(("- ", "##")):
+                # fix notes that aren't bulleted
+                line = "- " + line
+            if line.startswith("##"):
+                line = "\n#" + line
+            md_out.append(line)
+            # print(line, end="")
+
+    # rprint(md_out)
+    return md_out
+
+
 def main(REPORT_DIR: str = REPORT_DIR):
-    logger.info("-- Entering main.")
+    logger.info("-- Entering main...")
+    report_path = Path(REPORT_DIR)
 
     # for each report
-    reports = sorted(glob.glob(os.path.join(REPORT_DIR, "????.md")))
+    reports = sorted(report_path.glob("????.md"))
     logger.info(f"> Processing {len(reports)} reports...")
+
+    for report in reports:
+        # e.g. if report = /Users/djo/dev/au/au_diss/reports/1001.md
+        report_num = report.stem  # 1001
+        report_dir = report.parent.name  # reports
+        md_file = report.name  # 1001.md
+        # md_file_path = str(report)  # /Users/djo/dev/au/au_diss/reports/1001.md
+
+        # extract script comments and feedback from hand-transcribed trial notes
+        trial_notes = extract_comments(report)
 
     # extract data from xml
     # save csv (xls?)
